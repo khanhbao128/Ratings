56837e99d57cd67c5833199914fe91e6bff27af0
Hackbright Student <no-reply@hackbrightacademy.com>
2019-05-09 17:47:33 -0700

    User detail's and Movie detail's page added

--- server.py
+++ server.py
@@ -69,7 +69,7 @@ def allow_login():
         flash("You are Logged in")
         user_id=user.user_id
         session['user_id']=user_id
-        return redirect('/')
+        return redirect('/users/<user_id>')
     else:
         flash('Password incorrect')
         return redirect('/login')
@@ -83,16 +83,39 @@ def log_out():
     flash("Logged out")
 
 
-
-
-
-
 @app.route('/users')
 def user_list():
     """Show list of users."""
 
     users = User.query.all()
     return render_template("user_list.html", users=users)
+
+
+@app.route('/users/<user_id>')
+def user_detail(user_id):
+    """Show details of particular User"""
+
+    user = User.query.get(user_id)
+    
+    return render_template("user.html", user=user)
+                                        # score=score)     
+@app.route('/movies')
+def movie_list():
+    """Show list of users."""
+
+
+    movie = Movie.query.order_by(Movie.title).all()
+
+
+    return render_template("movie_list.html", movie=movie)
+
+@app.route('/movies/<movie_id>')
+def movie_detail(movie_id):
+    """Show list of movies"""
+    movie = Movie.query.get(movie_id)
+    return render_template("movie.html", movie=movie)
+
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
